377e11730bb70ee17a7a4d37ec719848ec7b85cf
huy <dinhtanhuy547@gmail.com>
2025-06-08 17:31:22 +0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,12 @@
+//Mã nguồn được chia sẻ youtube SOURCE ALL
+module.exports = {
+	'url': 'mongodb://127.0.0.1:27017',
+	'options': {
+		'dbName': 'rikfan', //
+		//'dbName': 'GAME', //
+		//'dbName': 'admin', //
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
